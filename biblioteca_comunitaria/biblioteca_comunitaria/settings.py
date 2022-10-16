91b918b6b8fc66e2f4076917864ae08fb95c6512
sthaynny <igorsthaynny@gmail.com>
2022-10-16 14:45:23 -0300

    feat: adicionando apps nas configurações iniciais

--- biblioteca_comunitaria/biblioteca_comunitaria/settings.py
+++ biblioteca_comunitaria/biblioteca_comunitaria/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login.apps.LoginConfig',
+    'emprestimos.apps.EmprestimosConfig',
+    'livros.apps.LivrosConfig',
 ]
 
 MIDDLEWARE = [
