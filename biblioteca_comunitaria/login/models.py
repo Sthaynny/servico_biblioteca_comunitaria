201a95e0e85b3ad0e4f61b3ece3b915053d2e8df
sthaynny <igorsthaynny@gmail.com>
2022-10-16 14:59:37 -0300

    feat: criando model user

--- biblioteca_comunitaria/login/models.py
+++ biblioteca_comunitaria/login/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    user = models.CharField(max_length=15)
+    senha = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
