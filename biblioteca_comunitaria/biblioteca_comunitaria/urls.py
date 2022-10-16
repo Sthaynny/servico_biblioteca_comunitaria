dcd2e7077117268b2fd132a9960d8c943179759a
sthaynny <igorsthaynny@gmail.com>
2022-10-16 15:12:10 -0300

    feat: criando modelo livros

--- biblioteca_comunitaria/biblioteca_comunitaria/urls.py
+++ biblioteca_comunitaria/biblioteca_comunitaria/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include,path
+from django.urls import include, path
 
 urlpatterns = [    
-    path('login/', include('login.urls')),
-    path('emprestimos/', include('emprestimos.urls')),
+    ##path('login/', include('login.urls')),
+    ##path('emprestimos/', include('emprestimos.urls')),
     path('livros/', include('livros.urls')),
     path('admin/', admin.site.urls),
 ]
--- biblioteca_comunitaria/livros/models.py
+++ biblioteca_comunitaria/livros/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 from pyexpat import model
 
+
 # Create your models here.
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    img = models.CharField;
+    descricao =  models.CharField(max_length=2000)
+    ano = models.CharField(max_length=255)
