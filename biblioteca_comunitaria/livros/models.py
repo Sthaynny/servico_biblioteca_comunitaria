be8fcf705c0ea66a2a4d66cfe89b50ffbec065e5
sthaynny <igorsthaynny@gmail.com>
2022-10-20 19:33:28 -0300

    criando classe emprestimo

--- biblioteca_comunitaria/livros/models.py
+++ biblioteca_comunitaria/livros/models.py
@@ -7,4 +7,9 @@ class Livro(models.Model):
     titulo = models.CharField(max_length=255) 
     descricao =  models.CharField(max_length=1500)
     autor = models.CharField(max_length=255)
+    #imagem = models.FileField()
+
+class Emprestimo(models.Model):
+    idUsuario = models.IntegerField()
+    idLivros= models.IntegerField()
     
