0029b9f78e27d871d313d4ed1d78fcb8297e010c
sthaynny <igorsthaynny@gmail.com>
2022-10-22 14:47:30 -0300

    refactor: renomeando app

--- biblioteca_comunitaria/auth/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class User(models.Model):
-    user = models.CharField(max_length=15)
-    senha = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
--- biblioteca_comunitaria/biblioteca_comunitaria/settings.py
+++ biblioteca_comunitaria/biblioteca_comunitaria/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'livros.apps.LivrosConfig',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
