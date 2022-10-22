242f1b38c03c7172f72faab9d52e9e56b07a9dbe
sthaynny <igorsthaynny@gmail.com>
2022-10-22 14:49:30 -0300

    fix: ajustando import view login

--- biblioteca_comunitaria/account/views.py
+++ biblioteca_comunitaria/account/views.py
@@ -1,10 +1,9 @@
 from django.contrib.auth import authenticate, login
+# Create your views here.
+from django.contrib.auth.models import User
 from django.http import (HttpResponse, HttpResponseBadRequest,
                          HttpResponseNotFound, JsonResponse)
 from django.shortcuts import render
-
-# Create your views here.
-from .models import User
 
 
 def criarUsuario(request):
