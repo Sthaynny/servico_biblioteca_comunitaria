b57f668497a5100997261a5ead12f54e32c5c654
sthaynny <igorsthaynny@gmail.com>
2022-10-22 11:54:30 -0300

    feat: criando funções de ogin

--- biblioteca_comunitaria/login/views.py
+++ biblioteca_comunitaria/login/views.py
@@ -1,6 +1,25 @@
 
 
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 
 # Create your views here.
 from .models import User
+
+
+def criarUsuario(request):
+    user = User.objects.create_user(request.POST['usuario'], request.POST['email'], request.POST['senha']);
+    
+
+def my_view(request):
+    username = request.POST['usuario']
+    password = request.POST['senha']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            # Redirecione para uma página de sucesso.
+        else:
+            # Retorna uma mensagem de erro de 'conta desabilitada' .
+    else:
+        # Retorna uma mensagem de erro 'login inválido'.
