8f0c59380ed8eee4c615e82e843d33f759b87db4
sthaynny <igorsthaynny@gmail.com>
2022-11-16 19:29:58 -0300

    feat: criando endpoint usuario

--- app/serializers.py
+++ app/serializers.py
@@ -5,8 +5,17 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups' , 'is_superuser','is_staff' ]
-    
+        fields = ['url', 'username', 'email', 'groups' , 'is_superuser','is_staff' , 'password']
+
+    def create(self, validated_data): 
+        user = User.objects.create(
+        username=validated_data['username'], 
+        email=validated_data['email'],  
+        is_staff=True,
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
     
 
 
